9da9a8ef03c35ccd0ea8320665b6c503645d0ac3
Travo100 <Travo100@gmail.com>
2019-03-12 17:54:35 -0700

    Added the delete button

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,13 +14,24 @@
   database.ref("/users").on("child_added", function(snapshot){
       console.log(snapshot.key);
       console.log(snapshot.val());
+      var btn = $("<button>");
+      btn.attr("data-key", snapshot.key);
+      btn.addClass("btn btn-danger delete-btn");
+      btn.text("Delete");
       var tr = $("<tr>").append(
           $("<th>").text(snapshot.key).attr("scope", "row"),
           $("<td>").text(snapshot.val().username),
-          $("<td>").text(snapshot.val().favMovie)
-      );
+          $("<td>").text(snapshot.val().favMovie),
+          $("<td>").html(btn)
+      ).attr("id", snapshot.key);
       $("tbody").append(tr);
   });
+
+database.ref("/users").on("child_removed", function(snapshot){
+    console.log(snapshot.key);
+    console.log(snapshot.val());
+    $("#"+snapshot.key).remove();
+});
 
   $("#submit-user").on("submit", function(e){
         e.preventDefault();
@@ -30,4 +41,12 @@
         });
         $("#user-name").val("")
         $("#fav-movie").val("")
+  });
+
+  $(document).on("click", ".delete-btn", function(e){
+      e.preventDefault();
+      console.log($(this).attr("data-key"));
+      var userKey = $(this).attr("data-key");
+      // delete the record by key/id
+      database.ref("/users").child(userKey).remove();
   });
