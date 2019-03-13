0e30b9bd24c705d14f5c399f25019f0dbc719f1a
Travo100 <Travo100@gmail.com>
2019-03-12 18:16:11 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# firebase-extended-session-2019
+# firebase-extended-session-2019 🔥
 A firebase extended session for UCSD extension
+
+GitHub: https://github.com/Travo100/firebase-extended-session-2019
+
+Deployed Site: https://travo100.github.io/firebase-extended-session-2019/
+
+Video: https://s3-us-west-2.amazonaws.com/extended-sessions-cbc/february-2019-ft/07-firebase/lesson.mp4
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,48 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAh3NyH2pmVuD-esw2_Zl2XExy7xdiJOH8",
     authDomain: "fir-extended-session-54c35.firebaseapp.com",
     databaseURL: "https://fir-extended-session-54c35.firebaseio.com",
     projectId: "fir-extended-session-54c35",
     storageBucket: "fir-extended-session-54c35.appspot.com",
     messagingSenderId: "184704892134"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  database.ref("/users").on("child_added", function(snapshot){
-      console.log(snapshot.key);
-      console.log(snapshot.val());
-      var btn = $("<button>");
-      btn.attr("data-key", snapshot.key);
-      btn.addClass("btn btn-danger delete-btn");
-      btn.text("Delete");
-      var tr = $("<tr>").append(
-          $("<th>").text(snapshot.key).attr("scope", "row"),
-          $("<td>").text(snapshot.val().username),
-          $("<td>").text(snapshot.val().favMovie),
-          $("<td>").html(btn)
-      ).attr("id", snapshot.key);
-      $("tbody").append(tr);
-  });
-
-database.ref("/users").on("child_removed", function(snapshot){
-    console.log(snapshot.key);
-    console.log(snapshot.val());
-    $("#"+snapshot.key).remove();
+database.ref("/users").on("child_added", function (snapshot) {
+    var btn = $("<button>");
+    btn.attr("data-key", snapshot.key);
+    btn.addClass("btn btn-danger delete-btn");
+    btn.text("Delete");
+    var tr = $("<tr>").append(
+        $("<th>").text(snapshot.key).attr("scope", "row"),
+        $("<td>").text(snapshot.val().username),
+        $("<td>").text(snapshot.val().favMovie),
+        $("<td>").html(btn)
+    ).attr("id", snapshot.key);
+    $("tbody").append(tr);
 });
 
-  $("#submit-user").on("submit", function(e){
-        e.preventDefault();
-        database.ref("/users").push({
-            username: $("#user-name").val().trim(),
-            favMovie: $("#fav-movie").val().trim()
-        });
-        $("#user-name").val("")
-        $("#fav-movie").val("")
-  });
+database.ref("/users").on("child_removed", function (snapshot) {
+    // removes the element from the DOM by it's id
+    $("#" + snapshot.key).remove();
+});
 
-  $(document).on("click", ".delete-btn", function(e){
-      e.preventDefault();
-      console.log($(this).attr("data-key"));
-      var userKey = $(this).attr("data-key");
-      // delete the record by key/id
-      database.ref("/users").child(userKey).remove();
-  });
+$("#submit-user").on("submit", function (e) {
+    e.preventDefault();
+    database.ref("/users").push({
+        username: $("#user-name").val().trim(),
+        favMovie: $("#fav-movie").val().trim()
+    });
+    $("#user-name").val("")
+    $("#fav-movie").val("")
+});
+
+$(document).on("click", ".delete-btn", function (e) {
+    e.preventDefault();
+    var userKey = $(this).attr("data-key");
+    // delete the record by key/id
+    database.ref("/users").child(userKey).remove();
+});
